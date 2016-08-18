ca9535f10a5965669daca7ce4a6c97950a516a5a
Luo SiFang[罗思放] <lsf5369@apac.arcsoft.corp>
2016-08-18 12:04:45 +0800

    solve 51

--- src/algorithms/a51_NQueens/Solution.java
+++ src/algorithms/a51_NQueens/Solution.java
@@ -1,33 +1,72 @@
 package algorithms.a51_NQueens;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
 	int N;
+	List<List<String>> ans = new ArrayList<>();
 	
     public List<List<String>> solveNQueens(int n) {
     	N = n;
         int[] positions = new int[n];
         int[][] vis = new int[n][n];
-        
+        place(0, vis, positions);
+        return ans;
+    }
+    
+    public List<String> drawGraph(int[] positions) {
+    	char[][] graph = new char[N][N];
+    	List<String> res = new ArrayList<>();
+    	for (int i = 0; i < N; i++) {
+    		for (int j = 0; j < N; j++) {
+    			if (positions[i] == j) {
+    				graph[i][j] = 'Q';
+    			} else {
+					graph[i][j] = '.';
+				}
+    		}
+    		res.add(new String(graph[i]));
+    	}
+    	return res;
     }
     
     public void place(int row, int[][] vis, int[] positions) {
+    	if (row >= N) {
+    		ans.add(drawGraph(positions));
+    		return;
+    	}
     	for (int i = 0; i < N; i++) {
     		if (vis[row][i] == 0) {
     			vis[row][i] = 1;
     			positions[row] = i;
+    			List<Point> points = new ArrayList<>();
     			for (int j = 0; j < N; j++) {
     				for (int k = 0; k < N; k++) {
     					if (j == row || k == i || j + k == row + i || j - k == row - i) {
     						if (vis[j][k] == 0) {
-    							
+    							vis[j][k] = 2;
+    							points.add(new Point(j, k));
     						}
     					}
     				}
     			}
+    			place(row + 1, vis, positions);
+    			for (Point point : points) {
+    				vis[point.x][point.y] = 0;
+    			}
+    			vis[row][i] = 0;
     		}
     	}
+    }
+    
+    class Point {
+    	int x;
+    	int y;
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
     }
     
 }
