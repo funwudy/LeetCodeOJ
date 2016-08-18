313dff3b18762f812b2a1a82c73298989f9035c4
Luo SiFang[罗思放] <lsf5369@apac.arcsoft.corp>
2016-08-18 16:03:44 +0800

    solve 5

--- src/algorithms/a5_LongestPalindromicSubstring/Solution.java
+++ src/algorithms/a5_LongestPalindromicSubstring/Solution.java
@@ -1,23 +1,32 @@
 package algorithms.a5_LongestPalindromicSubstring;
 
 public class Solution {
-	public boolean isPalindromic(String s, int start, int end) {//end is the last + 1
-		for (int i = start, j = end - 1; i <= j; i++, j--) {
-			if (s.charAt(i) != s.charAt(j)) {
-				return false;
-			}
-		}
-		return true;
-	}
 	
     public String longestPalindrome(String s) {
-        for (int l = s.length(); l > 1; l--) {
-        	for (int i = 0; i + l <= s.length(); i++) {
-        		if (isPalindromic(s, i, i + l)) {
-        			return s.substring(i, i + l);
-        		}
-        	}
-        }
-        return s.substring(0, 1);
+    	int length = s.length();
+    	boolean[][] dp = new boolean[length][length];
+    	dp[0][0] = true;
+    	for (int i = 1; i < length; i++) {
+    		dp[i][i] = true;
+    		dp[i][i - 1] = true;
+    	}
+    	int maxLen = 1;
+    	String longest = s.substring(0, 1);
+    	for (int len = 2; len <= length; len++) {
+    		for (int i = 0; i + len <= length; i++) {
+    			dp[i][i + len - 1] = (dp[i + 1][i + len - 2] 
+    					&& s.charAt(i) == s.charAt(i + len - 1));
+    			if (dp[i][i + len - 1] && len > maxLen) {
+    				maxLen = len;
+    				longest = s.substring(i, i + len);
+    			}
+    		}
+    	}
+    	return longest;
+    }
+    
+    public static void main(String[] args) {
+    	Solution solution = new Solution();
+    	System.out.println(solution.longestPalindrome("ccccfefc"));
     }
 }
---
+++ src/algorithms/a5_LongestPalindromicSubstring/Solution2.java
@@ -0,0 +1,48 @@
+package algorithms.a5_LongestPalindromicSubstring;
+
+public class Solution2 {
+
+	public String longestPalindrome(String s) {
+		if (s == null || s.equals(""))
+			return null;
+		int length = s.length();
+		int maxLen = 1;
+		String longest = s.substring(0, 1);
+        for (int i = 0; i < length; i++) {
+        	int tmpMax = 1;
+        	int from = i, to = i + 1;
+        	for (int j = 1; i - j >= 0 && i + j < length; j++) {
+        		if (s.charAt(i - j) == s.charAt(i + j)) {
+        			tmpMax = 2 * j + 1;
+        			from = i - j;
+        			to = i + j + 1;
+        		} else {
+        			break;
+        		}
+        	}
+        	for (int j = 0; i - j >= 0 && i + 1 + j < length; j++) {
+        		if (s.charAt(i - j) == s.charAt(i + 1 + j)) {
+        			if (tmpMax < 2 * j + 2) {
+        				tmpMax = 2 * j + 2;
+        				from = i - j;
+        				to = i + 2 + j;
+        			}
+        		} else {
+        			break;
+        		}
+        	}
+        	if (maxLen < tmpMax) {
+        		maxLen = tmpMax;
+        		longest = s.substring(from, to);
+        	}
+        }
+        return longest;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution2 solution = new Solution2();
+    	System.out.println(solution.longestPalindrome("aaaa"));
+	}
+
+}
