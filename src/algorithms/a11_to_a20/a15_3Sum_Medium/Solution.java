c7cbe55b11b445387994a1ec50e19f5ba0602ee8
Fun <fun_wudy@163.com>
2016-08-21 20:36:50 +0800

    solve 15

---
+++ src/algorithms/a11_to_a20/a15_3Sum_Medium/Solution.java
@@ -0,0 +1,54 @@
+package algorithms.a11_to_a20.a15_3Sum_Medium;
+
+import java.util.*;
+
+/**
+ * Created by 31798 on 2016/8/21.
+ */
+public class Solution {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        if (nums[0] > 0 || nums[nums.length - 1] < 0) {
+            return new ArrayList<>();
+        }
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int l = i + 1, r = nums.length - 1; l < r; ) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum < 0) {
+                    l++;
+                } else if (sum > 0) {
+                    r--;
+                } else {
+                    List<Integer> tmp = new ArrayList<>();
+                    tmp.add(nums[i]);
+                    tmp.add(nums[l]);
+                    tmp.add(nums[r]);
+                    Collections.sort(tmp);
+                    ans.add(tmp);
+                    do {
+                        l++;
+                    } while (l < r && nums[l] == nums[l - 1]);
+                    do {
+                        r--;
+                    } while (l < r && nums[r] == nums[r + 1]);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> ans = solution.threeSum(nums);
+
+    }
+}
--- src/algorithms/a1_to_a10/a4_MedianofTwoSortedArrays_Hard/Solution.java
+++ src/algorithms/a1_to_a10/a4_MedianofTwoSortedArrays_Hard/Solution.java
@@ -3,44 +3,42 @@ package algorithms.a1_to_a10.a4_MedianofTwoSortedArrays_Hard;
 import java.util.Arrays;
 
 public class Solution {
-	public static double findKth(int[] nums1, int m, int[] nums2, int n, int k) {
-		if (m > n) {
-			return findKth(nums2, n, nums1, m, k);
-		}
-		if (m == 0) {
-			return nums2[k - 1];
-		}
-		if (k == 1) {
-			return Math.min(nums1[0], nums2[0]);
-		}
-		
-		int l1 = Math.min(m, k / 2);
-		int l2 = k - l1;
-		if (nums1[l1 - 1] < nums2[l2 - 1]) {
-			return findKth(Arrays.copyOfRange(nums1, l1, m), m - l1, nums2, n, k - l1);
-		}
-		else if (nums1[l1 - 1] > nums2[l2 - 1]) {
-			return findKth(nums1, m, Arrays.copyOfRange(nums2, l2 , n), n - l2, k - l2);
-		}
-		else {
-			return nums2[l2 - 1];
-		}
-	}
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-    	int total = nums1.length + nums2.length;
-        if((total & 0x1) == 1) {
-        	return findKth(nums1, nums1.length, nums2, nums2.length, (total >>> 1) + 1);
+    public static double findKth(int[] nums1, int m, int[] nums2, int n, int k) {
+        if (m > n) {
+            return findKth(nums2, n, nums1, m, k);
         }
-        else {
-        	return (findKth(nums1, nums1.length, nums2, nums2.length, total >>> 1) +
-        			findKth(nums1, nums1.length, nums2, nums2.length, (total >>> 1) + 1)) / 2; 
+        if (m == 0) {
+            return nums2[k - 1];
         }
-        
+        if (k == 1) {
+            return Math.min(nums1[0], nums2[0]);
+        }
+
+        int l1 = Math.min(m, k / 2);
+        int l2 = k - l1;
+        if (nums1[l1 - 1] < nums2[l2 - 1]) {
+            return findKth(Arrays.copyOfRange(nums1, l1, m), m - l1, nums2, n, k - l1);
+        } else if (nums1[l1 - 1] > nums2[l2 - 1]) {
+            return findKth(nums1, m, Arrays.copyOfRange(nums2, l2, n), n - l2, k - l2);
+        } else {
+            return nums2[l2 - 1];
+        }
     }
-    
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int total = nums1.length + nums2.length;
+        if ((total & 0x1) == 1) {
+            return findKth(nums1, nums1.length, nums2, nums2.length, (total >>> 1) + 1);
+        } else {
+            return (findKth(nums1, nums1.length, nums2, nums2.length, total >>> 1) +
+                    findKth(nums1, nums1.length, nums2, nums2.length, (total >>> 1) + 1)) / 2;
+        }
+
+    }
+
     public static void main(String[] args) {
-    	Solution solution = new Solution();
-    	int[] nums1 = {1,3}, nums2 = {};
-    	System.out.println(solution.findMedianSortedArrays(nums1, nums2));
+        Solution solution = new Solution();
+        int[] nums1 = {1, 3}, nums2 = {};
+        System.out.println(solution.findMedianSortedArrays(nums1, nums2));
     }
 }
