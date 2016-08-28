bb718e32251038d879fd2f1286bca89dd41762b4
Fun <fun_wudy@163.com>
2016-08-28 20:47:26 +0800

    to solve 56

---
+++ src/algorithms/a51_to_a100/a56_MergeIntervals_Hard/Interval.java
@@ -0,0 +1,19 @@
+package algorithms.a51_to_a100.a56_MergeIntervals_Hard;
+
+/**
+ * Created by 31798 on 2016/8/28.
+ */
+public class Interval {
+    int start;
+    int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
---
+++ src/algorithms/a51_to_a100/a56_MergeIntervals_Hard/Solution.java
@@ -0,0 +1,12 @@
+package algorithms.a51_to_a100.a56_MergeIntervals_Hard;
+
+import java.util.List;
+
+/**
+ * Created by 31798 on 2016/8/28.
+ */
+public class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+
+    }
+}
